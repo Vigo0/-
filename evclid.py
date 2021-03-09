ac86069b07057cbe3d3aa8783bec3162231f277e
Vigo0 <71030737+Vigo0@users.noreply.github.com>
2021-03-09 22:30:21 +0300

    Add files via upload

---
+++ evclid.py
@@ -0,0 +1,20 @@
+def ab(a):
+    if a<0:
+        return-a
+    else:
+        return a
+
+
+    
+def gcd (a,b):
+    a = ab(a)
+    b = ab(b)
+    if a*b ==0:
+        return a+b
+    else:
+        if a>b:
+           return gcd(a%b,b)
+        else:
+           return gcd(a,b%a)
+print("gcd(-30,33)=",gcd(-30,33))
+
