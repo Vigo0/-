6b8d7dffa0696cb5c48d79418fc43fae918b8e7e
Vigo0 <71030737+Vigo0@users.noreply.github.com>
2021-03-09 22:47:24 +0300

    Update evclid.py

--- evclid.py
+++ evclid.py
@@ -1,5 +1,14 @@
+def ab(a):
+    if a<0:
+        return-a
+    else:
+        return a
 
+
+    
 def gcd (a,b):
+    a = ab(a)
+    b = ab(b)
     if a*b ==0:
         return a+b
     else:
@@ -8,4 +17,5 @@ def gcd (a,b):
         else:
            return gcd(a,b%a)
 print("gcd(-30,33)=",gcd(-30,33))
+
 
