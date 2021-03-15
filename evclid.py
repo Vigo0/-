2af523677bf3ab2fc918473cf760813682619b87
Vigo0 <vladimirpopov003@bk.ru>
2021-03-15 18:29:04 +0300

    Update evclid.py

--- evclid.py
+++ evclid.py
@@ -1,15 +1,15 @@
+import math
+import numpy
 def gcd (a,b):
-    if a < 0 :
-        a*= -1
-    if b < 0 :
-        b*= -1
-    if a*b ==0:
-        return a+b
-    else:
-        if a>b:
-           return gcd(a%b,b)
-        else:
-           return gcd(a,b%a)
+  b=abs(b)
+  a=abs(a)
+  if a*b ==0:
+      return a+b
+  else:
+      if a>b:
+          return gcd(a%b,b)
+      else:
+          return gcd(a,b%a)
 print("gcd(-30,33)=",gcd(-30,33))
 
 
